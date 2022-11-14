e8ad29fc30bf1b6cfbf38c8c2021220d07eafe7f
mati-fp <matipoletto@hotmail.com>
2022-11-14 01:04:07 -0300

    Algumas alterações do main

--- tripoDaBarbaridade/src/App.java
+++ tripoDaBarbaridade/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        //Se for fazer alguma alteração amigo Giovani cria uma nova branch com o nome da alteração que será feita, e depois quando
+        //ta rolando merjea.
         ReadFile arquivo = new ReadFile();
 
         arquivo.loadProgram("triboViking.txt");
